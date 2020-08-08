a815ffe787d2ac75848102e5f003d560451a5c3f
Eric Snyder <esnyder232@gmail.com>
2020-08-07 22:00:43 -0400

    matter tutorial checkin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # phaser-tilemap-tutorial5
+A tutorial that I'm following for phaser and tilemaps.
+
+Tutorial is here: https://itnext.io/modular-game-worlds-in-phaser-3-tilemaps-5-matter-physics-platformer-d14d1f614557
+Tutorial repo is here: https://github.com/mikewesthad/phaser-3-tilemap-blog-posts/tree/master/examples/post-5
+
+
--- src/scenes/game-manager-scene.js
+++ src/scenes/game-manager-scene.js
@@ -1,4 +1,4 @@
-import SimpleScene from "./simple-scene.js"
+import MainScene from "./main-scene.js"
 import GlobalFuncs from "../global-funcs.js"
 
 
@@ -14,8 +14,8 @@ export default class GameManagerScene extends Phaser.Scene {
 		console.log('adding scenes...');
 
 		//testing arcade physics
-		this.scene.add('simple-scene', SimpleScene);
-		this.scene.start('simple-scene');
+		this.scene.add('main-scene', MainScene);
+		this.scene.start('main-scene');
 
 
 		//some things to press and log stuff when i need to
